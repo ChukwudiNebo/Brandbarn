960a61f39f6b76c50f0652d76fbccf0ba9674bf8
chukwudi nebo <chukwudineboch@gmail.com>
2022-07-13 12:16:04 -0700

    second commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const connectDB = require('./config/db')
+
+
+const app = express();
+
+// Require cors
+var cors = require('cors')
+
+app.use(cors());
+
+
+// Connect Database
+connectDB();
+
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+
+
+//Define Routes
+app.use('/api/users', require('./route/api/user'));
+app.use('/api/auth', require('./route/api/auth'));
+// app.use('/api/profile', require('./route/api/profile'));
+// app.use('/api/posts', require('./route/api/posts'));
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );
+
+
